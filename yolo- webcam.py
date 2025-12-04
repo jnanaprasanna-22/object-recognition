827a5f319e134f984be471842a5e948b1c8608b4
Athikinasetti Jnana Lakshmi Prasanna <jnanalakshmiprasanna123@gmail.com>
2025-12-04 19:11:09 +0530

    Add files via upload

---
+++ yolo- webcam.py
@@ -0,0 +1,49 @@
+from torch.cpu import stream
+from flask import Flask, render_template_string, Response
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 480)
+
+model = YOLO('../yolo-weights/yolov8n.pt')
+classNames = [
+    "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light",
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+    "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa",
+    "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard",
+    "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+    "scissors", "teddy bear", "hair drier", "toothbrush"
+
+]
+while True:
+    success, image = cap.read()
+    result = model(image, conf=0.5, stream=True)
+    for r in result:
+        boxes = r.boxes
+        for box in boxes:
+            #Bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+           #cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(image,(x1, y1, w, h))
+            #Confidence
+            conf = math.ceil((box.conf[0]*100))/100
+
+            #Class Name
+            cls = int(box.cls[0])
+            print()
+            cvzone.putTextRect(image, f'{classNames[cls]}{conf}', (max(35, x1), max(35, y1)))
+    cv2.imshow(".jpg", image)
+    key = cv2.waitKey(3)
+    if key & 0xFF == ord('k'):
+        break
+cap.release()
+cv2.destroyAllWindows()
